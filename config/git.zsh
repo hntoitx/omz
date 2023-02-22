ebdf91576c5d74f5e52b3ff27ff7f049e6c9bc22
hi <1932328091@qq.com>
2023-02-22 17:05:25 +0800

    添加缓存文件夹,去除gam,修改gap先拉取再提交

--- config/git.zsh
+++ config/git.zsh
@@ -16,8 +16,8 @@ alias gss='git --no-pager status -s'
 alias gpt='git push origin --tags'
 alias glt='git tag -n --sort=taggerdate | tail -n ${1-10}'
 gat() { git tag -a $1 -m "$2" }
-gam() { git add --all && git commit -m "$*" }
-gap() { git add --all && git commit -m "$*" && git push }
+#gam() { git add --all && git commit -m "$*" }
+gap() { git pull && git add --all && git commit -m "$*" && git push }
 gitlog() {
     git --no-pager log --date=format:'%Y-%m-%d %H:%M'  --pretty=format:$1 --graph -n ${2-10} \
 }
